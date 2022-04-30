3094591c68ed92429e4b78e8c39505fa7d74fce8
pksingh17 <prabhakarsingh214@gmail.com>
2022-04-30 20:11:06 +0530

    Origin

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, SafeViewArea, Text, View, Platform } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
+      <SafeViewArea style={{ flex: 1 }} />
+      <View style={{ flex: 1, padding: 16, backgroundColor: "green" }}>
+        <Text>Search</Text>
+      </View>
+      <View style={{ flex: 1, padding: 16, backgroundColor: "blue" }}>
+        <Text>list</Text>
+      </View>
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
